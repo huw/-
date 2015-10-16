a269fa4c27e8726739e6b7612092ceb855e64cc6
huw <huw1999@gmail.com>
2015-10-16 19:46:07 +1100

    Add a deck of cards
    While this commit seems simple enough, a lot of work ultimately went into it, including some of my favourite code. Here's a quick list:
    
    - Add entitlements file (So that the app is appropriately sandboxed)
    - Rename the application to 'blackjack', while keeping the project named 'Blackjack'
    - Rename 'GameScene' to 'Game' (2 instances)
    - Add a Card class
        - This is an extension of `SKSpriteNode`, which is used to display image sprites. This may change in a future update.
        - The class file includes Suit and Rank, two enums which hold suit and rank data
            - Suit is either Spades, Hearts, Diamonds or Clubs, and can return the symbols for each type
            - Rank is the rank of a card, and can return that in a textual value (numerical will need to be determined later)
    - Build a deck of cards by iterating through Suit and Rank's `rawValue`s
    - Shuffle the deck of cards using GameplayKit
    - Add an algorithm for shuffling of my own creation to fulfill programming requirements
    
    The piece of code that I'm incredibly proud of is the whole Card class (it's really clean and organised), especially Rank.value().
    This function prints 'A', 'J', 'Q', or 'K' for special cards, and the value for individual cards—by converting the rawValue.
    This way, we cleverly minimise the need for a large amount of `case`s in our `switch`. Head into Card.swift and read it, seriously.

---
+++ blackjack/Card.swift
@@ -0,0 +1,90 @@
+//
+//  Card.swift
+//  Blackjack
+//
+//  Created by Huw on 2015-10-16.
+//  Copyright © 2015 Huw. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+/**
+This, and `Rank`, inherit from Int because Swift automatically assigns each a value in the order that they are allocated. With this, we can iterate over the enum to get a list of the possible cases.
+*/
+
+enum Suit: Int {
+    case Spades, Hearts, Diamonds, Clubs
+    
+    /**
+    - returns
+    Unicode value for the suit (♠, ♥, ♦, ♣)
+    */
+    
+    func symbol() -> Character {
+        switch self {
+        case Spades:
+            return "♠"
+        case Hearts:
+            return "♥"
+        case Diamonds:
+            return "♦"
+        case Clubs:
+            return "♣"
+        }
+    }
+}
+
+enum Rank: Int {
+    case Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    
+    /**
+    This is one of my favourite functions. Because each case is assigned a `rawValue` based on its allocation order, we don't have to write cases saying that `Two = "2"`, or however it's done. Instead, we can simply get the raw value, and add 1 to it. Convert that to a String, and we're done!
+    
+    - returns
+    String value to identify the rank of the card (2...10, J, Q, K, A)
+    */
+    
+    func value() -> String {
+        switch self {
+        case Ace:
+            return "A"
+        case Jack:
+            return "J"
+        case Queen:
+            return "Q"
+        case King:
+            return "K"
+        default:
+            return String(rawValue + 1)
+        }
+    }
+}
+
+class Card: SKSpriteNode {
+    
+    let suit: Suit
+    let rank: Rank
+    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+        
+        super.init(texture: nil, color: NSColor.whiteColor(), size: CGSizeZero)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /**
+    Each card should have a more readable value when printed. The default for SKSpriteNode will reveal its texture, size, and color—but this isn't useful. I mostly just want to see the cards. Instead, we can print out a nice value for the rank and suit of each card, the functions for which we define in `Rank` and `Card`.
+    
+    - returns
+    The value returned by Rank.value(), next to the value returned by Suit.symbol(), e.g. `K♣`, or `10♠`.
+    */
+    
+    override var description: String {
+        return "\(self.rank.value())\(self.suit.symbol())"
+    }
+}
---
+++ blackjack/Game.swift
@@ -0,0 +1,50 @@
+//
+//  GameScene.swift
+//  blackjack
+//
+//  Created by Huw on 2015-10-08.
+//  Copyright (c) 2015 Huw. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+        var deck: [Card] = []
+        
+        for suit_num in 0...3 {
+            
+            let suit = Suit(rawValue: suit_num)!
+            
+            for rank_num in 0...12 {
+                
+                let rank = Rank(rawValue: rank_num)!
+                deck.append(Card(suit: suit, rank: rank))
+            }
+        }
+        
+        /*
+        The decision was made here to use GameplayKit's algorithm over one I'd make myself. If I were to implement the Fisher-Yates shuffling algorithm myself, it would not be as efficient or effective as something that  make. However, because this is a programming class, and I'm supposed to do so, here is how I would build the Fisher-Yates algorithm:
+        
+            for pos1 in (0...52-1).reverse() {
+                let pos2 = Int(arc4random_uniform(UInt32(pos1) + 1))
+                let card1 = deck[pos1]
+                let card2 = deck[pos2]
+                deck[pos2] = card1
+                deck[pos1] = card2
+            }
+        
+        You may replace the code with this to verify that it works yourself.
+        */
+        
+        deck = GKMersenneTwisterRandomSource.sharedRandom().arrayByShufflingObjectsInArray(deck) as! [Card]
+    }
+    
+    override func mouseDown(theEvent: NSEvent) {
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+    }
+}
